60c726ed2da066a5c9175f138df0dd267dd1c41e
Tayyaba <tayyaba.afzal@gmail.com>
2024-03-06 13:16:56 +0000

    output folder created with html and css file. package json and jest installed. gitignore file created and node moudules added in it

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Team-Profile-Generator
+
+## Description:
+
+This app allows user to search for weather. The user can search for any city and will be able to to get forecast for the next 5 days. 
+
+## Installation:
+This app requires no installation. The quiz can be used easily run on the desktop or mobile browser. The app shows weather for the current day as well as for the next 5 days. The app also shows wind and humidity along with temperature. This app saves the data for previously viewed cities which allows user to acess the cities quicker. The saved locations can be cleared by clicking the 'clear history' button.   
+
+![Alt text](/assets/images/1.png)
+
+
+## Links:
+The link to the deployed app is below:
+
+https://tayyabaifzal.github.io/Weather-Dashboard/
+
+You can access the code via github repo linked below:
+
+https://github.com/tayyabaifzal/Weather-Dashboard
+
+## Contact
+For further suggestions or complaints about the app, contact via github repo shown below
+
+https://github.com/tayyabaifzal
+
+
+## License:
+MIT
--- index.js
+++ index.js
@@ -6,10 +6,232 @@ const path = require("path");
 const fs = require("fs");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+
+
+
+
+
+
+
+
+
+
+// const teamMembers = [];
+
+// const managerQuestons = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is the team manager's name?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return "Please enter the manager's name!";
+//             }
+//             return true;
+//         }
+
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is the team manager's id?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return "Please enter the manager's id!";
+//             }
+//             return true;
+//         }
+
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the team manager's email?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return "Please enter the manager's email!";
+//             }
+//             return true;
+//         }
+
+//     },
+//     {
+//         type: "input",
+//         name: "officeNumber",
+//         message: "What is the team manager's office number?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return "Please enter the manager's office number!";
+//             }
+//             return true;
+//         }
+
+//     }
+// ];
+
+// const engineerQuestions = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is the engineer's name?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the engineer's name!");
+//             }
+//             return true;
+//         }
+
+//     },
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is the engineer's id?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the engineer's id!");
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the engineer's email?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the engineer's email!");
+//             }
+//             return true;
+//         }
+//     },
+
+//     {
+//         type: "input",
+//         name: "github",
+//         message: "What is the engineer's github?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the engineer's github!");
+//             }
+//             return true;
+//         }
+//     }
+// ];
+
+
+// const internQuestions = [
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is the intern's name?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the intern's name!");
+//             }
+//             return true;
+//         }
+
+//     },
+
+//     {
+//         type: "input",
+//         name: "id",
+//         message: "What is the intern's id?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the intern's id!");
+//             }
+//             return true;
+//         }
+
+//     },
+//     {
+//         type: "input",
+//         name: "email",
+//         message: "What is the intern's email?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the intern's email!");
+//             }
+//             return true;
+//         }
+//     },
+//     {
+//         type: "input",
+//         name: "school",
+//         message: "What is the intern's school?",
+//         validate: answer => {
+//             if (answer === "") {
+//                 return ("Please enter the intern's school!");
+//             }
+//             return true;
+
+//         },
+
+//     }
+// ];
+
+// const addTeamMember = () => {
+//     inquirer.prompt([
+//         {
+//             type: "list",
+//             name: "addTeamMember",
+//             message: "What type of team member would you like to add?",
+//             choices: ["Engineer", "Intern", "I don't want to add any more team members"]
+//         }
+//     ])
+//         .then(answer => {
+//             if (answer.addteamMember === "Engineer") {
+//                 addEngineer();
+//             }
+//             else if (answer.addteamMember === "Intern") {
+//                 addIntern();
+//             }
+//             else {
+//                 console.log("No more team memners to add");
+//                 console.log(teamMembers);
+//                 console.log("Generating Team profile");
+//                 fs.writeFileSync(outputPath, render(teamMembers), "utf-8"); 
+//                 console.log("Team Profile Generator");
+//             }
+//         });
+// }
+
+// const addEngineer = () => {
+//     inquirer.prompt(engineerQuestions)
+//         .then(nameInput => {
+//             // const { name, id, email, github } = EngineerInput;
+//             const engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+//             teamMembers.push(engineer);
+//             addTeamMember();
+//         });
+// }
+
+// const addIntern = () => {
+//     inquirer.prompt(internQuestions)
+//         .then(answer => {
+//             const intern = new Intern(answer.name, answer.id, answer.email, answer.school);
+//             teamMembers.push(intern);
+//             addTeamMember();
+//         });
+// }
+
+// inti = () => {
+//     inquirer.prompt(managerQuestons)
+//         .then(answer => {
+//             const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+//             teamMembers.push(manager);
+//             addTeamMember();
+//         });
+// }
+
+// inti();
